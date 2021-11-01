091f555ff38a982c6120ada415fa8fb08a7f74fd
Hemanthsai G <ghshemanth@gmail.com>
2021-11-01 23:47:13 +0530

    v1.0.0

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template, redirect, url_for
-from flask_ngrok import run_with_ngrok
 import csv
 import pandas as pd
 import numpy as np
@@ -27,9 +26,15 @@ def find_member(roll_list, roll):
         return 0
 ##############################
 
+##############################
+seating_ids = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10', 'b11', 'b12', 'b13', 'b14', 'b15', 'b16', 'b17', 'b18', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13', 'c14', 'c15', 'c16', 'c17', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'd12', 'd13', 'd14', 'd15', 'd16', 'd17', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9', 'e10', 'e11', 'e12', 'e13', 'e14', 'e15', 'e16', 'e17', 'e18', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9', 'g10', 'g11', 'g12', 'g13', 'g14', 'g15', 'g16', 'g17', 'g18', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'h11', 'h12', 'h13', 'h14', 'h15', 'h16', 'h17', 'h18', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9', 'i10', 'i11', 'i12', 'i13', 'i14', 'i15', 'i16', 'i17', 'i18', 'j1', 'j2', 'j3', 'j4', 'j5', 'j6', 'j7', 'j8', 'j9', 'j10', 'j11', 'j12', 'j13', 'j14', 'j15', 'j16', 'j17', 'j18', 'k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'k8', 'k9', 'k10', 'k11', 'k12', 'k13', 'k14', 'k15', 'k16', 'k17', 'k18', 'l1', 'l2', 'l3', 'l4', 'l5', 'l6', 'l7', 'l8', 'l9', 'l10', 'l11', 'l12', 'l13', 'l14', 'l15', 'l16', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'm11', 'm12', 'm13', 'm14', 'm15', 'm16', 'm17', 'm18', 'm19', 'm20']
+##############################
+
+
+
+
 
 app = Flask(__name__)
-# run_with_ngrok(app)
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -52,7 +57,7 @@ def check():
         df1 = df1.drop(['Unnamed: 0'],axis=1)
     if result!=0:
         if dic["id"] in list(df1["Rolls"]):
-            result.append(list(df1.index[df1["Rolls"]==dic["id"]])[0])
+            result.append(seating_ids[list(df1.index[df1["Rolls"]==dic["id"]])[0]])
         else:
             result.append("Not allocated yet")
     nr=0
@@ -74,7 +79,6 @@ def verify():
         df1 = df1.drop(['Unnamed: 0'],axis=1)
 
 
-
     if dic["id"] not in list(df1["Rolls"]):
         if "empty" not in list(df1[df1['Branch']==dic["Branch"].lower()]['Rolls']):
                 print(dic["Branch"],"is full")
@@ -86,15 +90,16 @@ def verify():
                     df1["Rolls"][i] = dic["id"]
                     print(i)
                     df1.to_csv("final.csv",index=False)
-                    return render_template("index.html",allocated=i)
+                    result.append(seating_ids[i])
+                    return render_template("index.html",data=result)
                     # return render_template('index.html',data=result)
             
 
 
     else:
-        print("YOU HECK, DON'T FK***n MESS WITH ME. Your seat:",list(df1.index[df1["Rolls"]==dic["id"]])[0])
+        print("Your seat:",list(df1.index[df1["Rolls"]==dic["id"]])[0])
         
-        return render_template("index.html",allocated=list(df1.index[df1["Rolls"]==dic["id"]])[0])
+        return render_template("index.html",data=result,allocated=seating_ids[list(df1.index[df1["Rolls"]==dic["id"]])[0]])
     
     
     
@@ -104,3 +109,5 @@ def verify():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
+    # app.run()
+    
